3fbe1101f798432467b160858f5074f471cd2fa1
Guillermo <guillermo.garcia.espuela@gmail.com>
2021-04-18 14:17:29 +0200

    Pointed to buttons correctly

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
     chromeOptions.add_experimental_option("prefs", preferences)
     driver = webdriver.Chrome("chromedriver.exe",options=chromeOptions, desired_capabilities=chromeOptions.to_capabilities())
     driver.get("https://www.youtube.com/watch?v=2lAe1cqCOXo")
-    driver.find_elements_by_css_selector('.VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc IIdkle').click()
-    
+    driver.find_elements_by_class_name('lssxud')[1].click()
+    driver.find_element_by_id('dismiss-button').click()
