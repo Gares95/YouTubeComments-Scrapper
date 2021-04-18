679e7eeadf8470bcaf8c444756ba3b853756177e
Guillermo <guillermo.garcia.espuela@gmail.com>
2021-04-18 13:49:54 +0200

    get around cookies and user sign in messages

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     chromeOptions.add_argument('--ignore-certificate-errors')
     preferences = {"download.default_directory": "./"}
     chromeOptions.add_experimental_option("prefs", preferences)
-    driver = webdriver.Chrome("./chromerdriver.exe",options=chromeOptions, desired_capabilities=chromeOptions.to_capabilities())
+    driver = webdriver.Chrome("chromedriver.exe",options=chromeOptions, desired_capabilities=chromeOptions.to_capabilities())
     driver.get("https://www.youtube.com/watch?v=2lAe1cqCOXo")
+    driver.find_elements_by_css_selector('.VfPpkd-LgbsSe VfPpkd-LgbsSe-OWXEXe-k8QpJ VfPpkd-LgbsSe-OWXEXe-dgl2Hf nCP5yc AjY5Oe DuMIQc IIdkle').click()
     
